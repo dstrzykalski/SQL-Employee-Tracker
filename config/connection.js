1c6063c4bbd89c6c815dbedd2f8601429a14c9a6
dstrzykalski <dstrzykalski@yahoo.com>
2024-09-11 23:51:06 -0400

    Fixed! Needed to add a function requiring pg in the connection file and npm i dot env.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
-const Pg = require('pg');
 
-const connection = new Pg({
+const { Client } = require('pg');
+const connection = new Client({
     host: 'localhost',
-    port: 3001,
+    port: 5432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-    });
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database');
+});
 
 module.exports = connection;
